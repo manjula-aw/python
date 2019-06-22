193dee4580814f187e8e320c47fdba813fd40e67
manjula-aw <manjula.aw@gmail.com>
2019-06-22 18:03:28 +0800

    a simple script created for checking which TLDs are still available for registering your domain name.

---
+++ dns-availability-checker.py
@@ -0,0 +1,32 @@
+import socket
+import requests as req
+
+
+inputfilepath='tlds.txt'
+outputfilepath='unregistereddomains.txt'
+
+resp = req.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
+
+print(resp.text)
+
+# Note: Replace following value with your domain name (e.g. if you want to check for a domain for 'myblog' with any TLD, put myblog
+domainprefix='myblog'
+
+# Clears the content of output file.
+open(outputfilepath, 'w').close()
+
+for tld in resp.iter_lines():
+    
+   tld=tld.decode().lower().strip();
+   
+   if (len(tld) <= 3 and not tld.startswith('#') and not tld.startswith('xn--')):
+       
+       domainname = "{}.{}".format(domainprefix,tld);
+       try:
+           socket.gethostbyname_ex(domainname);
+       except:
+               print("{} is available.".format(domainname))
+               fout=open(outputfilepath,'a');
+               fout.write("{} is available.\n".format(domainname))
+               fout.close();
+                 
