bc2184a4411d210f363cb8b274b02f8ca0649729
manjula-aw <manjula.aw@gmail.com>
2019-06-24 14:08:32 +0800

    Update dns-availability-checker.py

--- dns-availability-checker.py
+++ dns-availability-checker.py
@@ -1,25 +1,19 @@
 import socket
 import requests as req
 
-
-outputfilepath='unregistereddomains.txt'
-
-resp = req.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
-
-print(resp.text)
-
 # Note: Replace following value with your domain name (e.g. if you want to check for a domain for 'myblog' with any TLD, put myblog
 domainprefix='myblog'
+
+outputfilepath="unregistereddomains-for-{}.txt".format(domainprefix);
+resp = req.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
 
 # Clears the content of output file.
 open(outputfilepath, 'w').close()
 
-for tld in resp.iter_lines():
-    
+for tld in resp.iter_lines():    
    tld=tld.decode().lower().strip();
    
-   if (len(tld) <= 3 and not tld.startswith('#') and not tld.startswith('xn--')):
-       
+   if (len(tld) <= 3 and not tld.startswith('#') and not tld.startswith('xn--')):       
        domainname = "{}.{}".format(domainprefix,tld);
        try:
            socket.gethostbyname_ex(domainname);
