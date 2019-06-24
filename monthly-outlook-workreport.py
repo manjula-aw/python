cf68ba69d88dcbde6e966b42ede4d8754e945b92
manjula-aw <manjula.aw@gmail.com>
2019-06-24 18:49:21 +0800

    Update monthly-outlook-workreport.py

--- monthly-outlook-workreport.py
+++ monthly-outlook-workreport.py
@@ -5,6 +5,7 @@ import os
 import sys
 import datetime
 import calendar
+import csv
 
 # Pre-requisite: install pywin32 using 'pip install pywin32'
 # Resources:
@@ -16,13 +17,14 @@ import calendar
 
 today = datetime.date.today()
 
-f = open("emailsummary-{}.csv".format(today.strftime("%b").upper()),"w")
+f = open("emailsummary-{}.csv".format(today.strftime("%b").upper()),"w", newline='')
+
+fieldnames = ['Time', 'Subject', 'Recipients']
+writer = csv.DictWriter(f, fieldnames=fieldnames)
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 sentmailbox=outlook.GetDefaultFolder(5).Items
 appointments=outlook.GetDefaultFolder(9).Items 
-
-
 
 firstdayofthismonth=today.replace(day=1).strftime("%d-%m-%y")
 firstdayofnextmonth=(today.replace(day=1) + datetime.timedelta(days=calendar.monthrange(today.year,today.month)[1])).strftime("%d-%m-%y")
@@ -32,13 +34,12 @@ restriction_meeting="[Start] > '{} 12:00 AM' And [End]  < '{} 12:00 AM'".format(
 
 messages=sentmailbox.restrict(restriction_mail);
 
-
-toprint="Sent time, subject, Recipients"
-print(toprint,file=f);
-print(toprint);
+writer.writeheader();
 
 for message in messages:
+    
     subject = message.Subject;
+    print(subject)
 
     if (not subject.startswith("Canceled:") and not subject.startswith("Accepted:") and not subject.startswith("Declined:")):
         try:
@@ -46,44 +47,39 @@ for message in messages:
             sender = str(message.Sender).strip();
             senttime = message.SentOn;
 
-            if (sender != recipients and not "@gmail.com" in recipients): 
-                toprint = "{},'{}',{}".format(senttime, subject, recipients);
-                print(toprint,file=f)
-                print(toprint)
+            if (sender != recipients and not "@gmail.com" in recipients):
+                writer.writerow({'Time': senttime, 'Subject': subject, 'Recipients':recipients });
                 
-        except com_error as e:
-            toprint = "{},'{}',{}".format("<encrypted email>", subject, "<encrypted email>");            
-            print(toprint,file=f);
-            print(toprint);
+        except com_error as e:           
+            writer.writerow({'Time': "<encrypted email>", 'Subject': subject, 'Recipients':"<encrypted email>" });
         except:            
             pass;
         
 
-# TODO: write the data to a excel file
 # TODO: organize data by week (both sent emails and outlook meetings togethere.)
+# TODO order calendar items by actual event date
 
-toprint="Meeting start time, subject, Organizer"
-print(toprint,file=f);
-print(toprint);
+# TODO: keep a list of people for each project to let script determine which email/meeting is for which project
+#	Recipient list: LastName FirstName; LastName FirstName
+#	Read from projec-data.json 
+
+writer.writeheader();
 
 restrictedItems = appointments.Restrict(restriction_meeting)
 
 for appointmentItem in restrictedItems:
+    
     subject = appointmentItem.Subject;
+    print(subject);
 
     if (not subject.startswith("Canceled:") and not subject.startswith("Accepted:") and not subject.startswith("Declined:")):
         try:
             organizer = str(appointmentItem.Organizer).strip();
             starttime = appointmentItem.Start;
-            
-            toprint = "{},'{}',{}".format(starttime, subject, organizer);
-            print(toprint,file=f)
-            print(toprint)
+            writer.writerow({'Time': starttime, 'Subject': subject, 'Recipients':organizer });
                 
         except com_error as e:
-            toprint = "{},'{}',{}".format("<encrypted email>", subject, "<encrypted email>");            
-            print(toprint,file=f);
-            print(toprint);
+            writer.writerow({'Time': "<encrypted email>", 'Subject': subject, 'Recipients':"<encrypted email>" });	
         except:            
             pass;
 		  
