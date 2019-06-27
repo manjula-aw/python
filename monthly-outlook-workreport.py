c5708426e3ab4364c2706c115d867300d6fee9ee
manjula-aw <manjula.aw@gmail.com>
2019-06-27 20:02:23 +0800

    Update monthly-outlook-workreport.py

--- monthly-outlook-workreport.py
+++ monthly-outlook-workreport.py
@@ -6,6 +6,7 @@ import sys
 import datetime
 import calendar
 import csv
+from collections import defaultdict
 
 # Pre-requisite: install pywin32 using 'pip install pywin32'
 # Resources:
@@ -34,50 +35,61 @@ restriction_meeting="[Start] > '{} 12:00 AM' And [End]  < '{} 12:00 AM'".format(
 
 messages=sentmailbox.restrict(restriction_mail);
 
+outputdata = defaultdict(lambda: defaultdict(list));
+
 writer.writeheader();
 
 for message in messages:
-    
+    print('.',end='');
     subject = message.Subject;
-    print(subject)
 
+    closestdate = '';
     if (not subject.startswith("Canceled:") and not subject.startswith("Accepted:") and not subject.startswith("Declined:")):
         try:
             recipients = str(message.To).strip();
             sender = str(message.Sender).strip();
-            senttime = message.SentOn.strftime("%Y-%m-%d %I%M");
+            senttime = message.SentOn.strftime("%Y-%m-%d %I:%M");
+            closestdate = message.SentOn.strftime("%Y-%m-%d");
 
             if (sender != recipients and not "@gmail.com" in recipients):
+                outputdata[closestdate]["emails"].append({'Time': senttime, 'Subject': subject, 'Recipients':recipients });
                 writer.writerow({'Time': senttime, 'Subject': subject, 'Recipients':recipients });
                 
-        except com_error as e:           
+        except com_error as e:
+            outputdata[closestdate]["emails"].append({'Time': "<encrypted email>", 'Subject': subject, 'Recipients':"<encrypted email>" });
             writer.writerow({'Time': "<encrypted email>", 'Subject': subject, 'Recipients':"<encrypted email>" });
         except:            
             pass;
         
 
-# TODO: organize data by week (both sent emails and outlook meetings togethere.)
-# TODO order calendar items by actual event date
+
 
 # TODO: keep a list of people for each project to let script determine which email/meeting is for which project
 #	Recipient list: LastName FirstName; LastName FirstName
 #	Read from projec-data.json 
 
+print()
 writer.writeheader();
 
 restrictedItems = appointments.Restrict(restriction_meeting)
 
+# TODO: get timing details of actual instance from recurring meetings.
+
 for appointmentItem in restrictedItems:
-    
+    print('.',end='');
     subject = appointmentItem.Subject;
-    print(subject);
+    closestdate = '';
 
     try:
         organizer = str(appointmentItem.Organizer).strip();
-        starttime = appointmentItem.Start.strftime("%Y-%m-%d %I%M");
+        starttime = appointmentItem.Start.strftime("%Y-%m-%d %I:%M");
+        closestdate = appointmentItem.Start.strftime("%Y-%m-%d");
+
+        outputdata[closestdate]["meetings"].append({'Time': starttime, 'Subject': subject, 'Recipients':organizer });
         writer.writerow({'Time': starttime, 'Subject': subject, 'Recipients':organizer });
             
     except com_error as e:
+        outputdata[closestdate]["meetings"].append({'Time': "<encrypted email>", 'Subject': subject, 'Recipients':"<encrypted email>" });
         writer.writerow({'Time': "<encrypted email>", 'Subject': subject, 'Recipients':"<encrypted email>" });	
     except:            
         pass;
@@ -85,3 +97,8 @@ for appointmentItem in restrictedItems:
 
 f.close()
 
+# TODO: organize data by week (both sent emails and outlook meetings togethere.)
+# TODO order calendar items by actual event date
+for i, va in outputdata.items():
+    print(i);
+    print(va);
